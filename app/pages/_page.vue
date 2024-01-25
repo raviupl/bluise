28555d57705b7c95be8deff8b407ec6677df724f
raviupl <77075228+raviupl@users.noreply.github.com>
2024-01-26 00:35:45 +0530

    Update _page.vue

--- app/pages/_page.vue
+++ app/pages/_page.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="page" :class="page.slug">
     <h1 class="page__title text-lg md:text-xl lg:text-4xl xl:text-6xl text-center py-8 md:py-16">
-      {{ page.title }}
+      {{ page.title }}1
     </h1>
 
     <div v-html="$md.render(page.content)" class="page__content markdown pt-4 md:pt-6 md:pb-24" />
